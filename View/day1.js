668f0ad57053097e827a16b062fb10216adc16a1
萧锐杰 <reijie_xiao@suishouji.com>
2017-01-24 17:52:15 +0800

    first day continute...

---
+++ View/day1.js
@@ -0,0 +1,4 @@
+'use strict';
+
+import React,{ Component } from 'react';
+import { Platform,ListView,StyleSheet,StatusBar,Text,TouchableHighlight,View } from 'react-native';
--- index.ios.js
+++ index.ios.js
@@ -4,50 +4,93 @@
  */
 'use strict';
 
-var React = require('react-native');
-var {
+import React, {Component} from 'react'
+import {
   AppRegistry,
+  DeviceEventEmitter,
+  Image,
+  Navigator,
+  ScrollView,
+  StatusBar,
   StyleSheet,
   Text,
-  View,
-} = React;
+  TouchableOpacity,
+  TouchableHighlight,
+  View
+}  from 'react-native';
 
-var AwesomeProject = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+
+class ThirtyDaysOfReactNative extends Component {
+    componentDidMount() {
+      StatusBar.setBarStyle(0);
+    }
+
+    configureScene(route, routeStack) {
+      if (route.type == 'Bottom') {
+        return Navigator.SceneConfigs.FloatFromBottom;
+      }
+      return Navigator.SceneConfigs.PushFromRight;
+    }
+
+    routeMapper = {
+      LeftButton: (route,navigator,index,navState) => {
+        if (route.index > 0) {
+          return <TouchableOpacity
+            underlayColor = 'transparent'
+            onProgress = {() => {if (index > 0) {navigator.pop()}}}>
+          </TouchableOpacity>
+        } else {
+          return null;
+        }
+      },
+      RightButton: (route,navigator,index,navState) => {
+        return null;
+      },
+      Title:(route,navigator,index,navState) => {
+        return (<Text style = {styles.navTitle}>{route.title}</Text>)
+      }
+    };
+
+    render() {
+      return (
+        <Navigator
+        initialRoute = {{
+          title: '30 Days of RN',
+          index: 0,
+          display: true
+          //component:MainVIew,
+        }}/>
+      )
+    }
+
+}
+function functionName() {
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flexGrow:1,
+  },
+  mainView:{
+    marginTop: 63
+  },
+  navBar:{
+    borderBottomWidth: 1,
+    bordetBottomColor:'#ddd',
+  },
+  navTitle: {
+    paddingtTop:10,
+    fontSize:18,
+    fontWeight:"500",
+  },
+
+  navBackBtn: {
+    paddingtTop: 10,
+    paddingtLeft: 10,
+    fontSize: 18,
+    color: "#555"
   }
-});
+})
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+AppRegistry.registerComponent('AwesomeProject', () => ThirtyDaysOfReactNative);
